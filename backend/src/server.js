5e71416eb29f1ac66e82f5c465295e605d2143e6
Wolo Gamers <wologamerz@gmail.com>
2025-08-14 15:01:54 +0700

    preparing deployment to prod

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,23 +3,38 @@ import notesRoutes from "./routes/notesRoutes.js";
 import connectDB from "./config/db.js";
 import dotenv from "dotenv";
 import rateLimiter from "./middlewares/rateLimiter.js";
+import path from "path";
 dotenv.config();
 
 const app = express();
+const __dirname = path.resolve();
 
 // middleware => is a function that runs  in the middle between the request and response
 app.use(express.json()); // transform all requests into json format, and get access to req.body
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
+if (process.env.NODE_ENV !== "production") {
+  app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Content-Type, Authorization"
+    );
+    next();
+  });
+}
 
 app.use(rateLimiter);
 
 app.use("/api/notes", notesRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
+}
 
 connectDB()
   .then(() => {
